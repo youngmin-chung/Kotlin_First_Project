18919e0cfeb6306a7e75f8d5f545a3329ca493e8
Youngmin Chung <y_chung2@fanshaweonline.ca>
2020-09-22 10:16:50 -0400

    branch number 2

--- app/src/main/java/com/example/mylinkedinproject01/MainActivity.kt
+++ app/src/main/java/com/example/mylinkedinproject01/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.mylinkedinproject01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "This is branch 2!", Toast.LENGTH_LONG).show()
     }
 }
